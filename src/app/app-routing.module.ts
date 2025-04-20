2d405ce52eddf3a6d78e78ab9ecf78167210961a
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-04-20 16:06:02 +0500

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ResultsComponent } from './components/results/results.component';
+import { AdsComponent } from './components/ads/ads.component';
+import { SectorsComponent } from './components/sectors/sectors.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { SocialsComponent } from './components/socials/socials.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaqComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContactComponent,
+    AboutComponent,
+    ServicesComponent,
+    ResultsComponent,
+    AdsComponent,
+    SectorsComponent,
+    VideosComponent,
+    SocialsComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
